5f446f13be79c59e410e7a1839ac850a5418f775
Mildness10 <m.akomoize@gmail.com>
2023-02-25 16:38:39 -0500

    added a function to check eligibility for SNAP

---
+++ access-assist/check_eligibility_snap.py
@@ -0,0 +1,18 @@
+def snap_eligibility(income_per_month, household):
+    if (income_per_month <= 2266) and (household == 1):
+        return True
+    elif (income_per_month <= 3052) and (household == 2):
+        return True
+    elif (income_per_month <= 3840) and (household == 3):
+        return True
+    elif (income_per_month <= 4626) and (household == 4):
+        return True
+    elif (income_per_month <= 5412) and (household == 5):
+        return True
+    elif (income_per_month <= 6200) and (household == 6):
+        return True
+    elif (income_per_month <= 6986) and (household == 7):
+        return True
+    return False
+
+print(snap_eligibility(3050, 2))
