8e617674e2765a7b18874c5f35db8c8683e2c838
XinyuP <xinyup@umich.edu>
2023-02-26 04:02:33 -0500

    pass response json list to MapComp

--- access-assist/src/components/map-comp/map-comp.component.jsx
+++ access-assist/src/components/map-comp/map-comp.component.jsx
@@ -1,13 +1,59 @@
 import React, { Fragment, useState } from 'react';
 import { AiOutlineArrowRight } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import GoogleMapReact from 'google-map-react';
 
 import './map-comp.styles.css';
 
+const google = window.google
+
+
+
+
+// Initialize and add the map
+const initMap = (props) => {
+    // The location of Uluru
+	const { responseData } = props;
+	console.log(responseData);
+
+    const current_loc = { lat: 42.3597232, lng: -71.0622357 }; // user input location 
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 8,
+      center: current_loc,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: current_loc,
+      map: map,
+    });
+  
+    // loop json, add marker
+  
+  
+  }
+  
+
+  
+  window.initMap = initMap;
+
+
+
 const MapComp = () => {
+
+
+
+
 	return (
 		<div className='map-comp-content'>
+
+
+
+
 			<p>MapComp Page</p>
+
+
+
 
 			<p>
 				Do you want to learn about state and federal resources you're eligible
