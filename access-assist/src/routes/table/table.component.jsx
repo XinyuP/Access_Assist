51e8e9856adbb4fa0419bd5eba6437837e5ac2d6
XinyuP <xinyup@umich.edu>
2023-02-26 10:05:25 -0500

    finalize version1.0

--- access-assist/src/routes/table/table.component.jsx
+++ access-assist/src/routes/table/table.component.jsx
@@ -1,7 +1,7 @@
 // import { Fragment } from 'react';
 import TableComp from '../../components/table-comp/table-comp.component';
 import { FcApproval } from 'react-icons/fc';
-import {ImCross}  from 'react-icons/im';
+import { ImCross } from 'react-icons/im';
 
 import React from 'react';
 import './table.styles.css';
@@ -21,36 +21,136 @@ const Table = () => {
 							<th class='tg-0pky'>Eligible</th>
 						</tr>
 					</thead>
-					
 
 					<tbody>
 						<tr>
 							<td class='tg-0pky'>1</td>
-							<td class='tg-0pky'></td>
-							<td class='tg-0pky' id="FcApproval"><FcApproval size={30}/></td>
-                        </tr>
-                        <tr>
+							<td class='tg-0pky'>
+								SNAP(Supplemental Nutrition Assistance Program)
+							</td>
+							<td class='tg-0pky' id='FcApproval'>
+								<FcApproval size={30} />
+							</td>
+						</tr>
+						<tr>
 							<td class='tg-0pky'>2</td>
-							<td class='tg-0pky'></td>
-							<td class='tg-0pky'><ImCross size={20}/></td>
+							<td class='tg-0pky'>Medicaid</td>
+							<td class='tg-0pky'>
+								<ImCross size={20} />
+							</td>
 						</tr>
 						<tr>
 							<td class='tg-0pky'>3</td>
-							<td class='tg-0pky'></td>
-							<td class='tg-0pky'><ImCross size={20}/></td>
+							<td class='tg-0pky'>
+								EAEDC (Emergency Aid to Elders, Disabled & Children)
+							</td>
+							<td class='tg-0pky'>
+								<ImCross size={20} />
+							</td>
 						</tr>
 						<tr>
 							<td class='tg-0pky'>4</td>
-							<td class='tg-0pky'></td>
-							<td class='tg-0pky'><FcApproval size={30}/></td>
+							<td class='tg-0pky'>MASSHealth</td>
+							<td class='tg-0pky'>
+								<FcApproval size={30} />
+							</td>
 						</tr>
+
+						<tr>
+							<td class='tg-0pky'>5</td>
+							<td class='tg-0pky'>Child Care Vouchers</td>
+							<td class='tg-0pky'>
+								<ImCross size={20} />
+							</td>
+						</tr>
+
+						<tr>
+							<td class='tg-0pky'>6</td>
+							<td class='tg-0pky'>Fuel Assistance</td>
+							<td class='tg-0pky'>
+								<FcApproval size={30} />
+							</td>
+						</tr>
+
+						<tr>
+							<td class='tg-0pky'>7</td>
+							<td class='tg-0pky'>Head Start</td>
+							<td class='tg-0pky'>
+								<FcApproval size={30} />
+							</td>
+						</tr>
+
+						<tr>
+							<td class='tg-0pky'>8</td>
+							<td class='tg-0pky'>SSI/SSDI</td>
+							<td class='tg-0pky'>
+								<ImCross size={20} />
+							</td>
+                        </tr>
+
+                        <tr>
+							<td class='tg-0pky'>9</td>
+							<td class='tg-0pky'>Haley House</td>
+							<td class='tg-0pky'>
+								<FcApproval size={30} />
+							</td>
+                        </tr>
+                        
+
+                        <tr>
+							<td class='tg-0pky'>10</td>
+							<td class='tg-0pky'>East Boston Community Soup</td>
+							<td class='tg-0pky'>
+								<FcApproval size={30} />
+							</td>
+                        </tr>
+                        
+                        <tr>
+							<td class='tg-0pky'>11</td>
+							<td class='tg-0pky'>Continuums of care</td>
+							<td class='tg-0pky'>
+								<ImCross size={20} />
+							</td>
+                        </tr>
+                        
+
+                        <tr>
+							<td class='tg-0pky'>12</td>
+							<td class='tg-0pky'>Boston University Upward Bound</td>
+							<td class='tg-0pky'>
+								<ImCross size={20} />
+							</td>
+                        </tr>
+                        
+                        <tr>
+							<td class='tg-0pky'>13</td>
+							<td class='tg-0pky'>Boston HERC</td>
+							<td class='tg-0pky'>
+								<FcApproval size={30} />
+							</td>
+                        </tr>
+                        
+                        <tr>
+							<td class='tg-0pky'>14</td>
+							<td class='tg-0pky'>Maternal support</td>
+							<td class='tg-0pky'>
+								<FcApproval size={30} />
+							</td>
+						</tr>
+                        
+
+
+
 					</tbody>
 				</table>
 			</div>
 		</div>
 
-		// </Fragment>
+		// </Fragment>TAFDC (Transitional Aid to Families with Dependent Children)
 	);
 };
 
 export default Table;
+
+
+
